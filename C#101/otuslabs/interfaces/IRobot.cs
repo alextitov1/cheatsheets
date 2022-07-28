d5494083a46c785bf0d520191b42b5789b81d271
AlexanderT <alexander.titov@asb.co.nz>
2022-07-28 23:29:40 +1200

    hw5 separate files for classes added

---
+++ C#101/otuslabs/interfaces/IRobot.cs
@@ -0,0 +1,16 @@
+﻿namespace interfaces.Robots
+{
+    public interface IRobot
+    {
+        public string GetInfo();
+
+        public List<string> GetComponents();
+
+        public string GetRobotType()
+        {
+            return "I'm a simple robot";
+        }
+
+    }
+}
+
--- C#101/otuslabs/interfaces/Program.cs
+++ C#101/otuslabs/interfaces/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text.Json;
+using interfaces.FlyingRobot;
+using interfaces.classQuadcopter;
 
 namespace interfaces;
 class Program
@@ -7,57 +9,7 @@ class Program
     {
         var MyRobot = new Quadcopter();
         Console.WriteLine($"Hello, World!, {((IFlyingRobot)MyRobot).GetRobotType()}");
-        Console.WriteLine($"I'm consist of {JsonSerializer.Serialize(MyRobot.GetComponents())} ");
+        Console.WriteLine($"I consist of {JsonSerializer.Serialize(MyRobot.GetComponents())} ");
         MyRobot.Charge();
-    }
-}
-
-public interface IRobot
-{
-    public string GetInfo();
-
-    public List<string> GetComponents();
-
-    public string GetRobotType()
-    {
-        return "I'm a simple robot";
-    }
-
-}
-
-public interface IChargeable
-{
-    public void Charge();
-
-    public string GetInfo();
-}
-
-public interface IFlyingRobot : IRobot
-{
-    public new string GetRobotType()
-    {
-        return "I'm a flying robot";
-    }
-}
-
-class Quadcopter : IFlyingRobot, IChargeable
-{
-    private List<string> _component = new List<string> { "rotor1", "rotor2", "rotor3", "rotor4" };
-
-    public List<string> GetComponents()
-    {
-        return _component;
-    }
-
-    public string GetInfo()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Charge()
-    {
-        Console.WriteLine("Charging...");
-        Thread.Sleep(3000);
-        Console.WriteLine("Charged");
     }
 }
